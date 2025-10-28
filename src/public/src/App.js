56b91787a250d7ca1a6bfcaf3f4c2340c34cf705
Palakkumari68 <palakkumari12124@gmail.com>
2025-10-28 22:50:21 +0530

    Create App.js

---
+++ src/public/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "60px" }}>
+      <h1>🚀 React App Dockerized with Multi-Stage Build</h1>
+      <p>This app is served using Nginx!</p>
+    </div>
+  );
+}
+
+export default App;
